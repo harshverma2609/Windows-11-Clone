0d642b3c2680450686355b47ba60d9dfaf068e55
harshverma2609 <115971718+harshverma2609@users.noreply.github.com>
2024-06-08 15:25:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ function startTimer() {
     setInterval(changeWallpaper, 10000);
     setInterval(dateTime,1000);
     getWeather();
+    startMenu.style.bottom = "-650px"
+    widgetMenu.style.left = "-1950px"
+    searchMenu.style.bottom = "-650px"
 }
 
 // LIVE DATE TIME
@@ -27,6 +30,27 @@ function dateTime(){
     document.getElementById("time").innerHTML=hour + ":" + minute;
     document.getElementById("date").innerHTML=date + "-" + month + "-" + year;
 }
+
+// TO HIDE SHOW FUNCTION
+document.getElementById('calc').style.display="none";
+document.getElementById('weather').style.display="none";
+document.getElementById('calendar').style.display="none";
+// THE getElementsByClassName RETURNS A COLLECTION OF ITEMS AND COZ OF THIS EACH ITEM MUST BE ITERATED BY FOR LOOP TO HIDE
+
+function display(id){
+    var x=document.getElementById(id);
+    if(x.style.display==="none"){
+        startMenu.style.bottom="-650px"
+        widgetMenu.style.left="-1950px"
+        searchMenu.style.bottom="-650px"
+        // document.getElementById('calc').style.display="none";
+        // document.getElementById('weather').style.display="none";
+        x.style.display="block";
+    }
+    else{
+        x.style.display="none";
+    }
+};
 
 // WEATHER API
 function getWeather(){
@@ -70,7 +94,8 @@ function displayWeather(data){
 
     if(data.cod === '404'){
         weatherInfoDiv.innerHTML=`<p>${data.message}</p>`
-    } else {
+    }
+    else{
         const cityName=data.name;
         const temperature=Math.round(data.main.temp - 273.15);
         const description=data.weather[0].description;
@@ -93,7 +118,7 @@ function displayWeather(data){
 }
 function displayHourlyForecast(hourlyData){
     const hourlyForecastDiv=document.getElementById('hourly-forecast');
-    const next24Hours=hourlyData.slice(0,8);
+    const next24Hours=hourlyData.slice(0,7);
 
     next24Hours.forEach(item => {
         const dateTime=new Date(item.dt*1000);
@@ -114,41 +139,101 @@ function displayHourlyForecast(hourlyData){
 }
 
 // CALCULATOR
-function Solve(val) {
-    var input = document.getElementById('res');
-    input.value += val;
+function Solve(val){
+    var input=document.getElementById('res');
+    input.value+=val;
 }
-function Result() {
-    var num1 = document.getElementById('res').value;
-    var num2 = eval(num1);
-    document.getElementById('res').value = num2;
+function Result(){
+    var num1=document.getElementById('res').value;
+    var num2=eval(num1);
+    document.getElementById('res').value=num2;
 }
-function Clear() {
-    var input = document.getElementById('res');
-    input.value = '';
+function Clear(){
+    var input=document.getElementById('res');
+    input.value='';
 }
-function Back() {
-    var input = document.getElementById('res');
-    input.value = input.value.slice(0,-1);
+function Back(){
+    var input=document.getElementById('res');
+    input.value=input.value.slice(0,-1);
 }
 
-// TO HIDE SHOW FUNCTION
-document.getElementById('calc').style.display="none";
-document.getElementById('weather').style.display="none";
+// CALENDER
+const header=document.querySelector("#calendar h3");
+const dates=document.querySelector(".dates");
+const navs=document.querySelectorAll("#prev, #next");
 
-function display(id){
-    var x = document.getElementById(id);
-    if (x.style.display === "none") {
-        startMenu.style.bottom = "-650px"
-        widgetMenu.style.left = "-950px"
-        searchMenu.style.bottom = "-650px"
-        // document.getElementById('calc').style.display="none";
-        // document.getElementById('weather').style.display="none";
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
+const months=[
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let date=new Date();
+let month=date.getMonth();
+let year=date.getFullYear();
+
+function renderCalendar(){
+  const start=new Date(year, month, 1).getDay();
+  const endDate=new Date(year, month + 1, 0).getDate();
+  const end=new Date(year, month, endDate).getDay();
+  const endDatePrev=new Date(year, month, 0).getDate();
+
+  let datesHtml="";
+
+  for(let i=start; i>0; i--){
+    datesHtml += `<li class="inactive">${endDatePrev - i + 1}</li>`;
+  }
+
+  for(let day=1; day<=endDate; day++){
+    let isToday =
+      day === new Date().getDate() &&
+      month === new Date().getMonth() &&
+      year === new Date().getFullYear();
+  
+    let className = isToday ? ' class="today"' : "";
+    datesHtml += `<li${className}>${day}</li>`;
+  }
+
+  for(let i=end; i<6; i++){
+    datesHtml += `<li class="inactive">${i - end + 1}</li>`;
+  }
+
+  dates.innerHTML=datesHtml;
+  header.textContent = `${months[month]} ${year}`;
+}
+navs.forEach((nav) =>{
+  nav.addEventListener("click", (e)=>{
+    const btnId=e.target.id;
+
+    if(btnId==="prev" && month===0){
+      year--;
+      month = 11;
     }
-};
+    else if(btnId==="next" && month===11){
+      year++;
+      month=0;
+    }
+    else{
+      month = btnId === "next" ? month + 1 : month - 1;
+    }
+
+    date=new Date(year, month, new Date().getDate());
+    year=date.getFullYear();
+    month=date.getMonth();
+
+    renderCalendar();
+  });
+});
+renderCalendar();
 
 // FUNCTIONALITY TO ICONS
 let startButton = document.getElementById("start");
@@ -160,10 +245,11 @@ let widgetMenu = document.getElementsByClassName("widgetmenu")[0];
 
 startButton.addEventListener("click",()=>{
     if(startMenu.style.bottom == "-650px"){
-        widgetMenu.style.left = "-950px"
+        widgetMenu.style.left = "-1950px"
         searchMenu.style.bottom = "-650px"
         document.getElementById('calc').style.display="none";
         document.getElementById('weather').style.display="none";
+        document.getElementById('calendar').style.display="none";
         startMenu.style.bottom = "55px"
     }else{
         startMenu.style.bottom = "-650px"
@@ -172,10 +258,11 @@ startButton.addEventListener("click",()=>{
 
 searchButton.addEventListener("click",()=>{
     if(searchMenu.style.bottom == "-650px"){
-        widgetMenu.style.left = "-950px"
+        widgetMenu.style.left = "-1950px"
         startMenu.style.bottom = "-650px"
         document.getElementById('calc').style.display="none";
         document.getElementById('weather').style.display="none";
+        document.getElementById('calendar').style.display="none";
         searchMenu.style.bottom = "55px"
     }else{
         searchMenu.style.bottom = "-650px"
@@ -183,14 +270,15 @@ searchButton.addEventListener("click",()=>{
 })
 
 widgetButton.addEventListener("click",()=>{
-    if(widgetMenu.style.left == "-950px"){
+    if(widgetMenu.style.left == "-1950px"){
         startMenu.style.bottom = "-650px"
         searchMenu.style.bottom = "-650px"
         document.getElementById('calc').style.display="none";
         document.getElementById('weather').style.display="none";
+        document.getElementById('calendar').style.display="none";
         widgetMenu.style.left = "0px"
     }else{
-        widgetMenu.style.left = "-950px"
+        widgetMenu.style.left = "-1950px"
     }
 })
 
